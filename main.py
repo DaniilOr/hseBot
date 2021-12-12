e2c327dd14a98749a2488ef97429f01636f0e876
Daniil <36066254+DaniilOr@users.noreply.github.com>
2021-12-12 21:10:02 +0600

    change contents

--- main.py
+++ main.py
@@ -1,223 +1,251 @@
-import parser
-
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.utils.markdown import text, bold, italic, code, pre
-import config
-import buttons
+import telebot
+import parse
+import json
+import stats
 import utils
-from parser import get_data, filter_response
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    ConversationHandler,
+)
+from telebot import types
+from typing import Any
 
-bot = Bot(token=config.API)
-dp = Dispatcher(bot, storage=MemoryStorage())
-dp.middleware.setup(LoggingMiddleware())
+UPLOAD, POSTPROCESSING = range(2)
 
-class Exchanging(StatesGroup):
-    waiting_for_sum = State()
-    waiting_for_name = State()
-    waiting_for_first_name = State()
-    waiting_for_second_name = State()
-    op_type = None
-    currency = None
-    currency1 = None
-    currency2 = None
+API_TOKEN = "5099641786:AAH6fMeMuWDQbu7n5tKlXq_v2iXVYscwlpE"
 
-@dp.callback_query_handler(lambda x: x.data == 'cancel')
-async def usd(callback_query: types.CallbackQuery, state: FSMContext):
-    Exchanging.op_type = None
-    await state.finish()
+bot = telebot.TeleBot(API_TOKEN, parse_mode=None)
 
 
-@dp.callback_query_handler(lambda x: x.data == 'USD')
-async def usd(callback_query: types.CallbackQuery):
-    await Exchanging.waiting_for_name.set()
-    await bot.send_message(chat_id=callback_query.from_user.id, text="Введите валюту")
-    Exchanging.op_type = callback_query.data + "T"
+def send_welcome(update: {types.Message}, _: Any) -> int:
+    """
+    Welcomes the user. Returns the next step
+
+    :param update: {message}
+    :param _: Any
+    """
+    update.message.reply_text("Привет! Я - бот для небольшой помощи с финансовыми расчетами.\nЯ могу дать отчет по .csv файлам с data.nasdaq.com\n\
+Отправь max/min/avg/med/std и можешь добавить временые рамки в формате \"FROM TO\", где FROM и TO - даты в формате YYYY-MM-DD. Также можно удалить файлы командой delete или попросить меня начертить график командой plot")
+    return UPLOAD
 
 
-@dp.callback_query_handler(lambda x: x.data == 'ETH', state='*')
-async def eth(callback_query: types.CallbackQuery):
-    await Exchanging.waiting_for_name.set()
-    await bot.send_message(chat_id=callback_query.from_user.id, text="Введите валюту")
-    Exchanging.op_type = callback_query.data
+def handle_docs(update: {types.Message}, _:Any) -> int:
+    """
+    This function uploads user date.
 
-
-@dp.callback_query_handler(lambda x: x.data == 'BTC', state='*')
-async def btc(callback_query: types.CallbackQuery):
-    await Exchanging.waiting_for_name.set()
-    await bot.send_message(chat_id=callback_query.from_user.id, text="Введите валюту")
-    Exchanging.op_type = callback_query.data
-
-
-@dp.callback_query_handler(lambda x: x.data == 'other', state='*')
-async def other(callback_query: types.CallbackQuery):
-    await Exchanging.waiting_for_first_name.set()
-    await bot.send_message(chat_id=callback_query.from_user.id, text="Введите первую валюту")
-    Exchanging.op_type = callback_query.data
-
-
-@dp.message_handler(state=Exchanging.waiting_for_name)
-async def read_name(message: types.Message,  state: FSMContext):
-    name = message.text.upper().strip()
-    Exchanging.currency = name
-    available = parser.check_name(name)
-    if not available:
-        await message.reply("Такой валюты нет")
-        Exchanging.op_type = None
-        await state.finish()
+    :param updated: Message - updated wrapper of message
+    """
+    file_info = bot.get_file(update.message.document.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    if update.message.document.mime_type == "application/json":
+        csv_file = parse.json_to_csv(downloaded_file, update.message.from_user.id)
     else:
-        await message.reply("Введите сумму")
-        await Exchanging.waiting_for_sum.set()
-
-
-@dp.message_handler(state=Exchanging.waiting_for_first_name)
-async def read_sum(message: types.Message,  state: FSMContext):
-    name = message.text.upper().strip()
-    Exchanging.currency = name
-    available = parser.check_name(name)
-    if not available:
-        await message.reply("Такой валюты нет")
-        Exchanging.op_type = None
-        await state.finish()
+        csv_file = parse.save_csv(downloaded_file, update.message.from_user.id)
+    if csv_file is None:
+        update.message.reply_text("Что-то пошло не так. Это точно верный файл?")
+        return UPLOAD
     else:
-        await message.reply("Введите вторую валюту")
-        await Exchanging.waiting_for_second_name.set()
-        Exchanging.currency1 = name
+        update.message.reply_text("Файл сохранен, теперь можно получать из него информацию")
+    return POSTPROCESSING
 
 
-@dp.message_handler(state=Exchanging.waiting_for_second_name)
-async def read_sum(message: types.Message,  state: FSMContext):
-    name = message.text.upper().strip()
-    Exchanging.currency = name
-    available = parser.check_name(name)
-    if not available and not name in ["USD", "BTC", "ETH"]:
-        await message.reply("Такой валюты нет")
-        Exchanging.op_type = None
-        await state.finish()
+def cancel(update: {types.Message}, _:Any)->int:
+    """
+    This function stops conversation.
+
+    :param updated: Message - updated wrapper of message
+    """
+    update.message.reply_text(
+        'Мое дело предложить - Ваше отказаться'
+        ' Будет нужна помощь - пиши.',
+    )
+    return ConversationHandler.END
+
+
+def delete(update: {types.Message}, _:Any)->int:
+    """
+    This function deletes user files from the system.
+
+    :param updated: Message - updated wrapper of message
+    """
+    res = utils.delete_file(update.message.from_user.id)
+    if res:
+        update.message.reply_text(
+            'Ваши данные удалены. Спасибо за использование бота!',
+        )
     else:
-        await message.reply("Введите сумму")
-        await Exchanging.waiting_for_sum.set()
-        Exchanging.currency2 = name
+        update.message.reply_text(
+            'А удалять-то  нечего.',
+        )
+    return ConversationHandler.END
 
 
-@dp.message_handler(state=Exchanging.waiting_for_sum)
-async def read_sum(message: types.Message,  state: FSMContext):
-    try:
-        sum = int(message.text)
-    except:
-        await message.reply("Сумма должна быть числом", reply_markup=buttons.CANCEL)
-        await state.finish()
+def find_min(updated: {types.Message}, _:Any)->int:
+    """
+    This function sends min to the user.
 
+    :param updated: Message - updated wrapper of message
+    """
+    if len(updated.message.text.split()) == 1:
+        updated.message.reply_text("Считаем минимум на всем диапазоне данных")
+        res = stats.calculate_min(updated.message.from_user.id)
+    elif len(updated.message.text.split()) == 3 and parse.is_date(updated.message.text.split()[1]) and \
+            parse.is_date(updated.message.text.split()[2]):
+        updated.message.reply_text("Считаем минимум на диапазоне между указанными датами")
+        _, start, end = updated.message.text.split()
+        res = stats.calculate_min(updated.message.from_user.id, start, end)
     else:
-        if Exchanging.op_type == "other":
-            await bot.send_message(chat_id=message.chat.id,
-                                   text=parser.convert(Exchanging.currency1, Exchanging.currency2, sum))
-        else:
-            await bot.send_message(chat_id=message.chat.id, text=parser.convert(Exchanging.op_type,
-                                                                                  Exchanging.currency,sum))
-        await state.finish()
+        updated.message.reply_text("Введите просто /min или /min с указанием двух дат (Y-m-d)")
+        return POSTPROCESSING
+    if res is None:
+        updated.message.reply_text("Вы точно ввели даты в формате Y-m-d? А файл точно не удаляли?")
+        return POSTPROCESSING
+    updated.message.reply_text(f"Min = {res}")
+    return POSTPROCESSING
 
 
+def find_max(updated: {types.Message}, _:Any)->int:
+    """
+    This function sends max to the user.
 
-@dp.message_handler(commands=['start'])
-async def process_start_command(message: types.Message):
-
-    await bot.send_message(chat_id=message.chat.id, text="Привет!\nЯ бот для любителей криптовалюты. Надеюсь тебе\
-                                                         понравится мой функционал.")
-
-
-@dp.message_handler(commands=['help'])
-async def process_help_command(message: types.Message):
-    await message.reply(text=text("Вот маленький список того что я умею делать:\n", "1. Я могу показать тебе топ-N криптовалют",
-                            "\n2. Я могу конвертировать разные валюты", "\n3. Я могу показать наглядную диаграму",
-                            "\n4. Я могу дать информацию о последних твитах Илона Маска, чтобы ты мог оценить их криптовалютные риски"))
-
-
-@dp.message_handler(commands=['top'])
-async def get_topN(message: types.Message):
-    command = message.get_full_command()
-    if len(command) == 1:
-        await message.reply("Пожалуйста, укажи сколько коинов нужно показать")
-        return
-    N = command[1]
-    try:
-        N = int(N)
-    except:
-        await message.reply("В комманде нужно указать число")
-        return
-    if N <= 0:
-        await message.reply("Не думаю, что я могу показать отрицательное число коинов")
-        return
-    all_data = get_data()
-    needed_coins = filter_response(all_data, N)
-    if len(needed_coins) < N:
-        await bot.send_message(chat_id=message.chat.id, text= f"Удалось найти всего {len(needed_coins)} коинов")
+    :param updated: Message - updated wrapper of message
+    """
+    if len(updated.message.text.split()) == 1:
+        updated.message.reply_text("Считаем максимум на всем диапазоне данных")
+        res = stats.calculate_max(updated.message.from_user.id)
+    elif len(updated.message.text.split()) == 3 and parse.is_date(updated.message.text.split()[1]) and \
+            parse.is_date(updated.message.text.split()[2]):
+        updated.message.reply_text("Считаем максимум на диапазоне между указанными датами")
+        _, start, end = updated.message.text.split()
+        res = stats.calculate_max(updated.message.from_user.id, start, end)
     else:
-        await bot.send_message(chat_id=message.chat.id, text= f"Вот {N} топовых коинов по отношению цены к BTC")
-    await bot.send_message(chat_id=message.chat.id, text=utils.prettify_coins(needed_coins))
+        updated.message.reply_text("Введите просто /max или /max с указанием двух дат (Y-m-d)")
+        return POSTPROCESSING
+    if res is None:
+        updated.message.reply_text("Вы точно ввели даты в формате Y-m-d? А файл точно не удаляли?")
+        return POSTPROCESSING
+    updated.message.reply_text(f"Max = {res}")
+    return POSTPROCESSING
 
 
-@dp.message_handler(commands=["exchange"])
-async def exchange(message: types.Message):
-    await message.reply("Что будем менять?", reply_markup=buttons.kb)
+def find_avg(updated: {types.Message}, _:Any)->int:
+    """
+    This function sends mean to the user.
+
+    :param updated: Message - updated wrapper of message
+    """
+    if len(updated.message.text.split()) == 1:
+        updated.message.reply_text("Считаем среднее на всем диапазоне данных")
+        res = stats.calculate_avg(updated.message.from_user.id)
+    elif len(updated.message.text.split()) == 3 and parse.is_date(updated.message.text.split()[1]) and \
+            parse.is_date(updated.message.text.split()[2]):
+        updated.message.reply_text("Считаем среднее на диапазоне между указанными датами")
+        _, start, end = updated.message.text.split()
+        res = stats.calculate_avg(updated.message.from_user.id, start, end)
+    else:
+        updated.message.reply_text("Введите просто /avg или /max с указанием двух дат (Y-m-d)")
+        return POSTPROCESSING
+    if res is None:
+        updated.message.reply_text("Вы точно ввели даты в формате Y-m-d? А файл точно не удаляли?")
+        return POSTPROCESSING
+    updated.message.reply_text(f"Avg = {res}")
+    return POSTPROCESSING
 
 
-@dp.message_handler(commands=["news"])
-async def exchange(message: types.Message):
-    command = message.get_full_command()
-    if len(command) == 1:
-        await message.reply("Пожалуйста, укажи сколько статей нужно показать")
-        return
-    N = command[1]
-    try:
-        N = int(N)
-    except:
-        await message.reply("В комманде нужно указать число")
-        return
-    if N <= 0:
-        await message.reply("Не думаю, что я могу показать отрицательное число статей")
-        return
-    articles = parser.parse_news(N)
-    for article in articles:
-        button = InlineKeyboardButton("Источник", url=article["link"])
-        mk = InlineKeyboardMarkup().add(button)
-        await bot.send_photo(chat_id=message.chat.id, photo=article["img"], caption=article["text"],
-                             reply_markup=mk)
+def find_median(updated: {types.Message}, _:Any)->int:
+    """
+    This function sends median to the user.
+
+    :param updated: Message - updated wrapper of message
+    """
+    if len(updated.message.text.split()) == 1:
+        updated.message.reply_text("Считаем медиану на всем диапазоне данных")
+        res = stats.calculate_median(updated.message.from_user.id)
+    elif len(updated.message.text.split()) == 3 and parse.is_date(updated.message.text.split()[1]) and \
+            parse.is_date(updated.message.text.split()[2]):
+        updated.message.reply_text("Считаем среднее на диапазоне между указанными датами")
+        _, start, end = updated.message.text.split()
+        res = stats.calculate_median(updated.message.from_user.id, start, end)
+    else:
+        updated.message.reply_text("Введите просто /avg или /avg с указанием двух дат (Y-m-d)")
+        return POSTPROCESSING
+    if res is None:
+        updated.message.reply_text("Вы точно ввели даты в формате Y-m-d? А файл точно не удаляли?")
+        return POSTPROCESSING
+    updated.message.reply_text(f"Median = {res}")
+    return POSTPROCESSING
 
 
-@dp.message_handler(commands=["check"])
-async def exchange(message: types.Message):
-    command = message.get_full_command()
-    if len(command) == 1:
-        await message.reply("Надо было указать еще и хэш транзакции")
-        return
-    await message.reply("Транзакция прошла успешно" if parser.check_transaction(hash) else "Транзакция не прошла")
+def find_std(updated: {types.Message}, _:Any)->int:
+    """
+    This function sends standard deviation to the user.
+
+    :param updated: Message - updated wrapper of message
+    """
+    if len(updated.message.text.split()) == 1:
+        updated.message.reply_text("Считаем среднее на всем диапазоне данных")
+        res = stats.calculate_avg(updated.message.from_user.id)
+    elif len(updated.message.text.split()) == 3 and parse.is_date(updated.message.text.split()[1]) and \
+            parse.is_date(updated.message.text.split()[2]):
+        updated.message.reply_text("Считаем средне-квадратичное отклонение на диапазоне между указанными датами")
+        _, start, end = updated.message.text.split()
+        res = stats.calculate_avg(updated.message.from_user.id, start, end)
+    else:
+        updated.message.reply_text("Введите просто /std или /std с указанием двух дат (Y-m-d)")
+        return POSTPROCESSING
+    if res is None:
+        updated.message.reply_text("Вы точно ввели даты в формате Y-m-d? А файл точно не удаляли?")
+        return POSTPROCESSING
+    updated.message.reply_text(f"Std = {res}")
+    return POSTPROCESSING
 
 
-@dp.message_handler(commands=["hist"])
-async def exchange(message: types.Message):
-    command = message.get_full_command()
-    if len(command) == 1:
-        await message.reply("Пожалуйста, укажи за сколько дней нужен график")
-        return
-    N = command[1]
-    try:
-        N = int(N)
-    except:
-        await message.reply("В комманде нужно указать число")
-        return
-    if int(N) <= 0:
-        await message.reply("Не думаю, что я могу показать отрицательное число дней")
-        return
-    image = parser.get_historicalbtc(N)
-    await bot.send_photo(chat_id=message.chat.id, photo=image)
+def plot(updated: {types.Message}, _:Any)->int:
+    """
+    This function sends the user a plot of exchange dynamics.
+
+    :param updated: Message - updated wrapper of message
+    """
+    if len(updated.message.text.split()) == 1:
+        updated.message.reply_text("Строим график на всем диапазоне данных")
+        res = stats.plot_dinamics(updated.message.from_user.id)
+    elif len(updated.message.text.split()) == 3 and parse.is_date(updated.message.text.split()[1]) and \
+            parse.is_date(updated.message.text.split()[2]):
+        updated.message.reply_text("Строим график на диапазоне между указанными датами")
+        _, start, end = updated.message.text.split()
+        res = stats.plot_dinamics(updated.message.from_user.id, start, end)
+    else:
+        updated.message.reply_text("Введите просто /plot или /plot с указанием двух дат (Y-m-d)")
+        return POSTPROCESSING
+    if res is None:
+        updated.message.reply_text("Вы точно ввели даты в формате Y-m-d? А файл точно не удаляли?")
+        return POSTPROCESSING
+    bot.send_photo(updated.message.from_user.id, photo=open(f"files/{updated.message.from_user.id}.png", "rb"))
+    return POSTPROCESSING
 
 
 if __name__ == "__main__":
-    executor.start_polling(dp)
+    updater = Updater(API_TOKEN)
+    dispatcher = updater.dispatcher
+
+    conv_handler = ConversationHandler(  # здесь строится логика разговора
+        entry_points=[CommandHandler('start', send_welcome)],
+        states={
+            UPLOAD: [MessageHandler(Filters.document & (
+                        Filters.document.mime_type("application/json") | Filters.document.mime_type("text/csv")),
+                                    handle_docs)],
+            POSTPROCESSING: [CommandHandler('min', find_min), CommandHandler('max', find_max),
+                             CommandHandler('avg', find_avg),  CommandHandler('med', find_median),
+                             CommandHandler('std', find_std),  CommandHandler('plot', plot)
+                             ],
+        },
+        fallbacks=[CommandHandler('cancel', cancel), CommandHandler('delete', delete)],
+    )
+
+    dispatcher.add_handler(conv_handler)
+
+    updater.start_polling()
+    updater.idle()
